28c2b86d7d76a1a38481600c77f5fd9bbfaad2b3
avfisher <brianwangrewa@gmail.com>
2016-03-22 00:15:58 +0800

    Update WinSysHelper.bat

--- WinSysHelper.bat
+++ WinSysHelper.bat
@@ -7,15 +7,14 @@ rem ***********************************
 echo.
 echo ************** WinSystemHelper **************
 echo.
-rem systeminfo > tmp.txt
 for /f "delims=" %%a in ('ver') do call :.ver %%a
 
-if %winver%==5.0 set OSversion=WIN2000&&goto lt2003
-if %winver%==5.1 set OSversion=WINXP&&goto lt2003   
-if %winver%==5.2 set OSversion=WIN2003&&goto lt2003
-if %winver%==6.0 set OSversion=WIN2008&&goto gt2003 
-if %winver%==6.1 set OSversion=WIN2008R2&&goto gt2003
-if %winver%==6.2 set OSversion=WIN2012&&goto gt2003 
+if %winver%==5.0 set OSversion=Win_2000&&goto lt2003
+if %winver%==5.1 set OSversion=Win_XP&&goto lt2003   
+if %winver%==5.2 set OSversion=Win_2003&&goto lt2003
+if %winver%==6.0 set OSversion=Win_2008/Win_Vesta&&goto gt2003 
+if %winver%==6.1 set OSversion=Win_2008_R2/Win_7&&goto gt2003
+if %winver%==6.2 set OSversion=Win_2012/Win_8&&goto gt2003 
 set OSversion=Unknown&&goto quit
 
 :.ver
@@ -25,21 +24,23 @@ set Winver=%Winver:~0,3%
 exit /b
 
 :lt2003
-for /f "tokens=1,2,3,4,* delims=," %%i in (explt2003.txt) do (
-	find %%i tmp.txt && echo %%i,"installed" ||echo %%i,"not installed"
-)
+echo OS: %OSversion%
+echo.
+echo Exploit(s) of Privilege Escalation:
+echo.
+systeminfo > tmp.txt && for /f "tokens=1,2,3,4,* delims=," %%i in (explt2003.txt) do (
+type tmp.txt|find /i "%%i"||echo vul: %%j, exp: %%l)
 goto quit
 
 :gt2003
-systeminfo > tmp.txt && for /f "tokens=1,2,3,4,* delims=," %%i in (expgt2003.txt) do (type tmp.txt|find /i "%%i"||echo %%i,%%j,"not installed")
+echo OS: %OSversion%
+echo.
+echo Exploit(s) of Privilege Escalation:
+echo.
+systeminfo > tmp.txt && for /f "tokens=1,2,3,4,* delims=," %%i in (expgt2003.txt) do (
+type tmp.txt|find /i "%%i"||echo vul: %%j, exp: %%l)
 goto quit
 
 :quit
-echo.
-echo ************** Cleaning files... **************
-echo.
-echo tmp.txt
 del /f /q /a tmp.txt
-echo.
-echo ***************** Clean done! *****************
 exit /b
